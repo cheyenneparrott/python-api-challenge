345f4940d4b019b2f9fc86d581c539af4700bfef
Cheyenne Parrott <cdorsey2@ku.edu>
2020-08-31 00:01:38 -0500

    Updated files and added PNG images

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "68e5bb3ba8ad9aae2e97ddfaa81ed940"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyAZAFWveZlatKaTyHMgeDCakGukecdCag0"
